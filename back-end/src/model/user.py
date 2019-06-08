1a0d263b599813048d8b9852ba7e518dc268460a
Prashannes <44089321+Prashannes@users.noreply.github.com>
2019-06-08 19:53:00 +0100

    Update user.py

--- back-end/src/model/user.py
+++ back-end/src/model/user.py
@@ -8,7 +8,7 @@ class User:
         self.nickname = nickname
 
     def __str__(self):
-        return f'User(email={self.email}, password={self.password}, nickname={self.nickname}'
+        return f'User(email={self.email}, password={self.password}, nickname={self.nickname})'
 
     @staticmethod
     def fromDict(userDict):
