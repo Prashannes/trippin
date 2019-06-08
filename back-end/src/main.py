d8027dbd02ecd0295a30f285fca807984eefcd18
Prashannes <44089321+Prashannes@users.noreply.github.com>
2019-06-08 19:28:48 +0100

    Update main.py

--- back-end/src/main.py
+++ back-end/src/main.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
 
     accountManager = AccountManager(database='database.db')
     api = V0Api(accountManager=accountManager)
-    api.run(host='146.169.47.204', debug=True)
+    api.run(host='0.0.0.0', debug=True)
 
