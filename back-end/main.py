af7cdddb8d10b3977cd0c4c1232b65f56b971512
Prashannes <44089321+Prashannes@users.noreply.github.com>
2019-06-08 18:28:35 +0100

    Update main.py

--- back-end/main.py
+++ back-end/main.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     # db = sqlite3.connect('database.db')
     # databaseSaver = AccountManager(db=db)
     api = V0Api()
-    api.run(host='146.169.47.204', debug=True)
+    api.run(host='0.0.0.0', debug=True)
 
