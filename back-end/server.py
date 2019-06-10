3bd6ddf7386f6bcb351869b71c791526a6f10ae9
root <root@ubu30gb-templ.doc.ic.ac.uk>
2019-06-10 22:46:57 +0100

    deletes trips upon completion

--- back-end/server.py
+++ back-end/server.py
@@ -15,61 +15,49 @@ def drop_trips_table():
   c.execute("DROP TABLE	trips")
 
 def create_accounts_table():
-  c.execute("CREATE TABLE IF NOT EXISTS accounts(email VARCHAR(255), password VARCHAR(255), nickname VARCHAR(255))")
+  c.execute("CREATE TABLE IF NOT EXISTS accounts(username VARCHAR(255), password VARCHAR(255))")
 
 def create_trips_table():
-  c.execute("CREATE TABLE IF NOT EXISTS trips(tripcode VARCHAR(10), email VARCHAR(50), long VARCHAR(50), lat VARCHAR(50), destLong VARCHAR(50), destLat VARCHAR(50), nickname VARCHAR(30) )")
+  c.execute("CREATE TABLE IF NOT EXISTS trips(tripcode VARCHAR(10), username VARCHAR(255), lat VARCHAR(50), long VARCHAR(50), destLat VARCHAR(50), destLong VARCHAR(50) )")
 
 def join_trip(trip):
   conn = sqlite3.connect('database.db')
   c = conn.cursor()
-  c.execute("INSERT INTO trips VALUES('" + trip['tripcode'] + "','" + trip['email'] + "','" + trip['long'] + "', '" + trip['lat'] + "','" + trip['destLong'] +"','" + trip['destLat'] +"','" + trip['nickname'] + "')")
+  c.execute("INSERT INTO trips VALUES('" + trip['tripcode'] + "','" + trip['username'] + "','" + trip['lat'] + "', '" + trip['long'] + "','" + trip['destLat'] +"','" + trip['destLong'] + "')")
   conn.commit() 
 
 def get_trip_info(trip):
   conn = sqlite3.connect('database.db')
   c = conn.cursor()
-  result = c.execute("SELECT email,long,lat,destLong,destLat,nickname FROM trips WHERE tripcode = '" + trip['tripcode'] + "' LIMIT 6")
+  result = c.execute("SELECT username, lat, long, destLat, destLong FROM trips WHERE tripcode = '" + trip['tripcode'] + "' LIMIT 6")
   conn.commit()
   return c.fetchall()
-
-def add_accounts_test_data():
-  conn = sqlite3.connect('database.db')
-  c = conn.cursor() 
-  c.execute("INSERT INTO accounts VALUES('user@example.com', 'password', 'usrrrr')")
-  conn.commit()
 
 def add_user(user):
   conn = sqlite3.connect('database.db')
   c = conn.cursor()
-  c.execute("INSERT INTO accounts VALUES('" + user['email'] + "','" + user['password'] + "','" + user['nickname'] + "')")
+  c.execute("INSERT INTO accounts VALUES('" + user['username'] + "','" + user['password']+ "')")
   conn.commit()
 
 def get_user(user):
   conn = sqlite3.connect('database.db')
   c = conn.cursor()
-  result = c.execute("SELECT email,nickname FROM accounts WHERE email = '" + user['email'] + "' AND password = '" + user['password'] + "' LIMIT 1")
+  result = c.execute("SELECT COUNT(username) FROM accounts WHERE username = '" + user['username'] + "' AND password = '" + user['password'] + "' LIMIT 1")
   conn.commit()
-  return c.fetchall()
+  return c.fetchall()   
 
-def delete_entry(id):
+def end_trip(user):
   conn = sqlite3.connect('database.db')
   c = conn.cursor()
-  #TODO
+  c.execute("DELETE FROM trips WHERE username = '" + user['username'] + "'")
   conn.commit()
-   
-def undelete_entry(id):
-  conn = sqlite3.connect('database.db')
-  c = conn.cursor()  
-  #TODO
-  conn.commit()
-   
 
-#drop_accounts_table()
+
 create_accounts_table()
 create_trips_table()
+#drop_accounts_table()
 #drop_trips_table()
-#add_accounts_test_data()
+#
 
 app = Flask(__name__)
 CORS(app)
@@ -77,20 +65,24 @@ CORS(app)
 @app.route('/accounts', methods=['GET', 'POST', 'PUT'])
 def index():
   if request.method == 'POST':
-    add_user({'email':request.values['email'], 'password':request.values['password'], 'nickname':request.values['nickname']})
+    add_user({'username':request.values['username'], 'password':request.values['password']})
     return Response("", status=201, mimetype='application/json')
   elif request.method == 'GET':
-    usr = json.dumps(get_user({'email':request.values['email'], 'password':request.values['password']}))
-    return Response(usr, status=200, mimetype='application/json')
+    resp = json.dumps(get_user({'username':request.values['username'], 'password':request.values['password']}))
+    return Response(resp, status=200, mimetype='application/json')
 
 @app.route('/trips', methods=['GET', 'POST', 'PUT'])
 def index2():
   if request.method == 'POST':
-    join_trip({'tripcode':request.values['tripcode'], 'email':request.values['email'], 'long':request.values['long'], 'lat':request.values['lat'], 'destLong':request.values['destLong'], 'destLat':request.values['destLat'], 'nickname':request.values['nickname']})
+    join_trip({'tripcode':request.values['tripcode'], 'username':request.values['username'], 'lat':request.values['lat'], 'long':request.values['long'], 'destLat':request.values['destLat'], 'destLong':request.values['destLong']})
     return Response("", status=201, mimetype='application/json')
   elif request.method == 'GET':
     pos = json.dumps(get_trip_info({'tripcode':request.values['tripcode']}))
     return Response(pos, status=200, mimetype='application/json')
+  elif request.method == 'PUT':
+    end_trip({'username':request.values['username']})
+    return Response("", status=200, mimetype='application/json')
+
 
 
 if __name__ == '__main__':
